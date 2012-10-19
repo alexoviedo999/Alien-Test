ae3f6cc9f1423dccafd820959ca0951f2717b39e
alexoviedo999 <alexoviedo999@gmail.com>
2012-10-19 10:06:35 -0400

    commit test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gittest::Application.config.secret_token = '3b7528834ce90ca13d6f6280cb358c441f510691d2c9d0e9f0938d9706b3d55aca9b198ed669b2054bbc0da31acf0888fda5fa42092b462965ea4ceb5df4589d'
---
+++ db/migrate/20121019135447_create_aliens.rb
@@ -0,0 +1,11 @@
+class CreateAliens < ActiveRecord::Migration
+  def change
+    create_table :aliens do |t|
+      t.string :name
+      t.string :string
+      t.string :photo
+
+      t.timestamps
+    end
+  end
+end
